1a7fc8cf3abd0c1f13630418737c409adb66af77
Helena Yankson <91065869+pgm-heleyank@users.noreply.github.com>
2022-12-23 13:38:02 +0100

    "added hit-test"

--- hit-test-v2/vite-project/js/ar.js
+++ hit-test-v2/vite-project/js/ar.js
@@ -4,15 +4,30 @@ const app = () => {
   };
 
   activateXR = async () => {
-    // create canvas and initialize Webgl Context
+    console.log("test");
+    // create canvas and initialize WebGL Context
     const canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
     const gl = canvas.getContext("webgl", { xrCompatible: true });
 
-    // create scene to draw object on canvas
+    // create scene to draw object on
     const scene = new THREE.Scene();
 
-    //materials for cube
+    const directionalLights = new THREE.DirectionalLight(0xffffff, 0.3);
+    directionalLights.position.set(10, 15, 10);
+
+    scene.add(directionalLights);
+
+    const loader = new THREE.GLTFLoader();
+    let reticle;
+    loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", function(gltf) {​​
+  reticle = gltf.scene;
+  reticle.visible = false;
+  scene.add(reticle);
+}​​)
+
+
+    // materials for cube
     const materials = [
       new THREE.MeshBasicMaterial({ color: "red" }),
       new THREE.MeshBasicMaterial({ color: "blue" }),
@@ -21,18 +36,13 @@ const app = () => {
       new THREE.MeshBasicMaterial({ color: "skyblue" }),
       new THREE.MeshBasicMaterial({ color: "darkblue" }),
     ];
+
     const cube = new THREE.Mesh(
       new THREE.BoxGeometry(0.2, 0.2, 0.2),
       materials
     );
-    cube.position.set(0, 0, -1);
-    scene.add(cube);
-
-    function rotateCube(speed) {
-      cube.rotation.x -= speed * 2;
-      cube.rotation.y -= speed;
-      cube.rotation.z -= speed * 3;
-    }
+    //cube.position.set(0, 0, -1);
+    //scene.add(cube);
 
     const renderer = new THREE.WebGLRenderer({
       alpha: true,
@@ -45,37 +55,62 @@ const app = () => {
 
     const camera = new THREE.PerspectiveCamera();
     camera.matrixAutoUpdate = false;
+    // try {
+    const session = await navigator.xr.requestSession("immersive-ar",{requiredFeatures: ["hit-test"]});
 
-    const session = await navigator.xr.requestSession("immersive-ar");
     session.updateRenderState({
       baseLayer: new XRWebGLLayer(session, gl),
     });
-
+   session.addEventListener('select', (event) => {
+    if(cube) {
+      const clone = cube.clone();
+      clone.position.copy(reticle.position);
+      scene.add(clone);
+    }
+   })
     const referenceSpace = await session.requestReferenceSpace("local");
 
+    const viewerSpace = await session.requestReferenceSpace('viewer');
+    const hitTestSource = await session.requestHitTestSource({space:viewerSpace});
     const onXRFrame = (time, frame) => {
       session.requestAnimationFrame(onXRFrame);
+
       gl.bindFramebuffer(
         gl.FRAMEBUFFER,
-        session.renderState.baseLayer.frameBuffer
+        session.renderState.baseLayer.framebuffer
       );
+
       const pose = frame.getViewerPose(referenceSpace);
 
       if (pose) {
+        rotateCube(cube, 0.01, 0.02);
+
         const view = pose.views[0];
+
         const viewport = session.renderState.baseLayer.getViewport(view);
         renderer.setSize(viewport.width, viewport.height);
 
         camera.matrix.fromArray(view.transform.matrix);
         camera.projectionMatrix.fromArray(view.projectionMatrix);
         camera.updateMatrixWorld(true);
-        rotateCube(0.1);
-
+        const hitTestResult=frame.getHitTestResult(hitTestSource)
+        if(hitTestResult.length > 0 && reticle) {
+          const hitPose = hitTestResult[0].getPose(referenceSpace);
+          reticle.visible=true;
+          reticle.position.set(hitPose.transform.position.x, hitPose.transform.position.y,hitPose.transform.position.z)
+          reticle.updateMatrixWorld = true;
+        }
         renderer.render(scene, camera);
       }
     };
     session.requestAnimationFrame(onXRFrame);
   };
+
+  rotateCube = (cubeToRotate, speedX = 0, speedY = 0) => {
+    cubeToRotate.rotation.y += speedX;
+    cubeToRotate.rotation.x += speedY;
+  };
+
   init();
 };
 
